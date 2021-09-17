27727857af6a7d850d50ebccd9181b918355ca84
Cam Appel <cameron@ourworldindata.org>
2021-09-16 20:09:04 -0400

    Update romania.R
    data(tests): Add antigen tests to Romania script

--- scripts/scripts/testing/automations/incremental/romania.R
+++ scripts/scripts/testing/automations/incremental/romania.R
@@ -1,35 +1,52 @@
-links <- read_html("https://gov.ro/ro/media/comunicate") %>%
-    html_nodes(".DescriptionList h2 a") %>%
-    html_attr("href")
-
+pcr <- c()
+ag <- c()
+date <- c()
+  
+link <- paste("https://gov.ro/ro/media/comunicate&page=4&page=",i,sep = "")
+  
+links <- read_html(link) %>%
+html_nodes(".DescriptionList h2 a") %>%
+html_attr("href")
+  
 url <- links[str_detect(links, "buletin")][1]
-
+  
 url <- read_html(url) %>%
-    html_node(".filesBoxInner a") %>%
-    html_attr("href")
-
+html_node(".filesBoxInner a") %>%
+html_attr("href")
+  
 date <- str_extract(url, "\\d+-\\d+_BULETIN") %>%
-    str_replace("BULETIN", as.character(year(today()))) %>%
-    dmy()
+  str_replace("BULETIN", as.character(year(today()))) %>%
+  dmy()
 if (is.na(date)) date <- today()
-
+  
 url <- str_replace(url, "http\\://https", "https")
-
+  
 download.file(url = url, destfile = "tmp/tmp.pdf", quiet = TRUE)
+  
+file <- pdf_text("tmp/tmp.pdf")
+  
+pcr <- file %>%
+  str_extract("Până la această dată, la nivel național, au fost prelucrate?.*") %>%
+    
+  na.omit() %>%
+  str_replace_all("[^\\d]", "") %>%
+  as.integer()
+  
+ag <- file %>%
+  str_extract("teste RT-PCR și?.*") %>%
+    
+  na.omit() %>%
+  str_replace_all("[^\\d]", "") %>%
+  as.integer()
 
-count <- pdf_text("tmp/tmp.pdf") %>%
-    str_extract("Până la această dată, la nivel național, au fost prelucrate?.*") %>%
-    na.omit() %>%
-    str_replace_all("[^\\d]", "") %>%
-    as.integer()
+count <- pcr + ag
 
 add_snapshot(
-    count = count,
-    date = date,
-    sheet_name = "Romania",
-    country = "Romania",
-    testing_type = "PCR only",
-    units = "tests performed",
-    source_url = url,
-    source_label = "Ministry of Internal Affairs"
-)
+  count = count,
+  date = date,
+  sheet_name = "Romania",
+  country = "Romania",
+  testing_type = "PCR only",
+  units = "tests performed",
+  source_url = url,
+  source_label = "Ministry of Internal Affairs"
